097bc77fb587fb88baabeecb0284b257670d9ae2
5 <gerardoemery@live.com.mx>
2020-07-01 00:18:37 -0500

    dash

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Image from "../assets/cafe1.jpg";
 import Sauce from "../assets/cafe2.jpg";
@@ -28,22 +27,27 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#328c99",
     position: "relative",
     margin: "auto",
-    width: 315,
-    height: 385,
+    width: 270,
+    height: 345,
     marginTop: 20,
     boxShadow: "none",
   },
   stripeContainer: {
     background: "#42296f",
     backgroundSize: "cover",
+    color: 'lightsteelblue',
+    borderBottomColor: '2px solid red',
     height: 50,
+    fontFamily: 'Oswald',
+    fontSize: 20,
     textAlign: "center",
   },
   stripe: {
-    fontFamily: 'Oswald',
-    fontSize: 25,
-    height: 40,
-    color: 'lightsteelblue'
+    
+    // fontSize: 25,
+    // height: 40,
+    // width: 100,
+    // color: 'lightsteelblue'
   }
 }));
 const Dashboard = () => {
@@ -51,34 +55,23 @@ const Dashboard = () => {
   return (
     <div className={classes.root}>
       <Grid container spacing={2}>
-        <Grid item xs={7}>
+        <Grid item xs={5} sm={6}>
           <Paper className={classes.saucerContainer}></Paper>
         </Grid>
-        <Grid item xs={5}>
+        <Grid item xs={7} sm={6}>
           <Paper className={classes.breakfastContainer}>
             <figure></figure>
           </Paper>
         </Grid>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
-              <path
-                fill='#328c99'
-                fill-opacity='1'
-                d='M0,160L48,176C96,192,192,224,288,234.7C384,245,480,235,576,197.3C672,160,768,96,864,74.7C960,53,1056,75,1152,96C1248,117,1344,139,1392,149.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z'
-              ></path>
-            </svg>
-          </Paper>
-        </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={12}>
           <Paper className={classes.stripeContainer}>
-            <Box display='flex' justifyContent='center'>
-              <Box border={1} borderColor='cadetblue' width="30%" className={classes.stripe} >
-              COME JOIN THE PARTY AT KALOC
-              </Box>
-            </Box>
+          COME JOIN THE PARTY AT KALOC</Paper>
+        </Grid> 
+        <Grid item xs={12} sm={12}>
+          <Paper >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#328c99" fill-opacity="1" d="M0,160L48,176C96,192,192,224,288,234.7C384,245,480,235,576,197.3C672,160,768,96,864,74.7C960,53,1056,75,1152,96C1248,117,1344,139,1392,149.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
           </Paper>
-        </Grid>
+        </Grid> 
       </Grid>
     </div>
   );
