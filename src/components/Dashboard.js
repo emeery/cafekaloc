84ae6dd7c7cb94c145bba3e5113f823a17855e34
5 <gerardoemery@live.com.mx>
2020-07-02 02:46:33 -0500

    xl

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,14 +5,14 @@ import Grid from "@material-ui/core/Grid";
 import Image from "../assets/cafe1.jpg";
 import Sauce from "../assets/cafe2.jpg";
 import Break from "../assets/Better_Breakfast.svg";
-import '../styles/dashboard.css'
+import "../styles/dashboard.css";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   scheduleContainer: {
     backgroundImage: `url(${Image})`,
-    backgroundSize: 'cover',
+    backgroundSize: "cover",
     backgroundPosition: "center",
     height: 410,
   },
@@ -35,28 +35,38 @@ const useStyles = makeStyles((theme) => ({
   stripeContainer: {
     background: "#42296f",
     backgroundSize: "cover",
-    color: 'lightsteelblue',
-    borderBottomColor: '2px solid red',
+    color: "lightsteelblue",
+    borderBottomColor: "2px solid red",
     height: 50,
-    fontFamily: 'Bebas Neue',
-    fontSize: '6vw',
+    fontFamily: "Bebas Neue",
+    fontSize: "6vw",
     textAlign: "center",
   },
   stripe: {
-    
-    // fontSize: 25,
-    // height: 40,
-    // width: 100,
-    // color: 'lightsteelblue'
-  }
+    background: 'none',
+    height: 80,
+    marginTop: 30,
+  },
+  transparent: {
+    background: "transparent",
+    fontFamily: "Oswald",
+    fontSize: 19,
+    color: 'aliceblue',
+    boxShadow: "none",
+    textAlign: "center",
+  },
+  column: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
 }));
 const Dashboard = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-
       <Grid container spacing={2}>
-      <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={6}>
           <Paper className={classes.scheduleContainer}></Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -64,9 +74,23 @@ const Dashboard = () => {
         </Grid>
         <Grid item xs={12} sm={12}>
           <Paper className={classes.stripeContainer}>
-          COME JOIN THE PARTY AT KALOC</Paper>
-        </Grid> 
-        
+            COME JOIN THE PARTY AT KALOC
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container className={classes.stripe}>
+        <Grid item xs={12} sm={6} md={3} lg={3} xl={3} className={classes.column}>
+          <Paper className={classes.transparent}>Política de Privacidad</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} lg={3} xl={3} className={classes.column}>
+          <Paper className={classes.transparent}>© Kaloc 2020</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} lg={3} xl={3} className={classes.column}>
+          <Paper className={classes.transparent}>Instagram</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} lg={3} xl={3} className={classes.column}>
+          <Paper className={classes.transparent}>Encuentra tu tienda</Paper>
+        </Grid>
       </Grid>
     </div>
   );
