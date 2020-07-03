67fe939310e15acfebd1ae9a3eb027595e7b4971
5 <gerardoemery@live.com.mx>
2020-07-03 02:11:49 -0500

    demo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,91 +1,55 @@
 import React from "react";
-import {
-    makeStyles
-} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Didi from "../assets/didifood.png";
+import Icon from "../assets/kaloc.png";
 const useStyles = makeStyles((theme) => ({
-    stripe: {
-        background: "#333740",
-        height: 120,
-        marginTop: 30,
-    },
-    transparent: {
-        background: "transparent",
-        fontFamily: "Bebas Neue",
-        fontSize: 22,
-        color: "turquoise",
-        boxShadow: "none",
-        textAlign: "center",
-    },
-    column: {
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        backgroundColor: "#333740",
-    },
-    didi: {
-        width: 85,
-        height: 70,
-        position: "relative",
-        margin: "auto",
-        background: `url(${Didi})`,
-        backgroundSize: "cover",
-    },
+  stripe: {
+    background: "#333740",
+    height: 120,
+    marginTop: 30,
+  },
+  transparent: {
+    background: "transparent",
+    fontFamily: "Bebas Neue",
+    fontSize: 22,
+    color: "turquoise",
+    boxShadow: "none",
+    textAlign: "center",
+  },
+  column: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "#333740",
+  },
+  didi: {
+    width: 85,
+    height: 70,
+    position: "relative",
+    margin: "auto",
+    background: `url(${Didi})`,
+    backgroundSize: "cover",
+  },
+  icon: {
+    width: 85,
+    height: 70,
+    position: "relative",
+    margin: "auto",
+    background: `url(${Icon})`,
+    backgroundSize: "cover",
+  },
 }));
 const Footer = () => {
-        const classes = useStyles();
-        return ( <
-                Grid container className = {
-                    classes.stripe
-                } >
-                <
-                Grid item xs = {
-                    12
-                }
-                sm = {
-                    6
-                }
-                md = {
-                    3
-                }
-                lg = {
-                    3
-                }
-                xl = {
-                    3
-                }
-                className = {
-                    classes.column
-                } >
-                <
-                div className = {
-                    classes.didi
-                } > < /div> < /
-                Grid > <
-                Grid item xs = {
-                    12
-                }
-                sm = {
-                    6
-                }
-                md = {
-                    3
-                }
-                lg = {
-                    3
-                }
-                xl = {
-                    3
-                }
-                className = {
-                    classes.column
-                } >
-                <
-                Paper className = {
-                    classes.transparent
-                } > ©KA `LOC 2020</Paper>
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.stripe}>
+      <Grid item xs={12} sm={6} md={3} lg={3} xl={3} className={classes.column}>
+        <div className={classes.didi}> </div>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3} lg={3} xl={3} className={classes.column}>
+        <div className={classes.icon}> </div>
       </Grid>
       <Grid item xs={12} sm={6} md={3} lg={3} xl={3} className={classes.column}>
         <Paper className={classes.transparent}>Instagram</Paper>
